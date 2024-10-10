bbaa063f4739ae185b8e93ea43ed294702e9ca25
KomaMix <ksabirov448@gmail.com>
2024-10-10 13:26:59 +0300

    Добавил необходимые классы

---
+++ CustomFileOpenerAndSaver/Models/TransferFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CustomFileOpenerAndSaver.Models
+{
+    public class TransferFile
+    {
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("extension")]
+        public string Extension { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("error")]
+        public Error Error { get; set; }
+    }
+}
